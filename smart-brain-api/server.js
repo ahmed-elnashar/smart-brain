367bccc210afaf2a0ee159f1b94c9a192fcb721b
ahmed-elnashar <a7medelnashar@hotmail.com>
2020-06-20 06:10:46 +0200

    added the backend server and controllers to smart-brain front end

---
+++ smart-brain-api/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: '',
+    password: '',
+    database: 'smart_brain',
+  },
+});
+
+db.select('*')
+  .from('users')
+  .then(data => {
+    console.log(data);
+  });
+
+const app = express();
+
+app.use(cors());
+// express builtin bodyparser to parse json
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});z
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.listen(4000, () => {
+  console.log('app is running on port 4000');
+});
